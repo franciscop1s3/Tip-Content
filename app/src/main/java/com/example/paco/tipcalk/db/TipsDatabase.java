a78e2d5a7bbcdf1df7219d6bbbd239492caaa0f2
francisco <jarioso13silva@gmail.com>
2016-11-03 08:53:27 -0600

    add ORM database Expecification

---
+++ app/src/main/java/com/example/paco/tipcalk/db/TipsDatabase.java
@@ -0,0 +1,10 @@
+package com.example.paco.tipcalk.db;
+
+/**
+ * Created by paco on 03/11/2016.
+ */
+@Database(name = TipsDatabase.NAME, version = TipsDatabase.VERSION)
+class TipsDatabase {
+    public static final String NAME = "Tips";
+    public static final int VERSION = 1;
+}
